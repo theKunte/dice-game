487f952f12a9f1eb312c11cdcd8adc1a2803e0d2
Jenny <jennykunte90@gmail.com>
2023-03-27 21:22:22 -0700

    init yaahtzee game

---
+++ yaahtzee-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import HomeView from './components/HomeView';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+      <HomeView/>
+    </div>
+  );
+}
+
+export default App;
---
+++ yaahtzee-app/src/components/HomeView/index.js
@@ -0,0 +1,12 @@
+import './index.css';
+
+const HomeView =() => {
+    return (
+        <div className='home-view'>
+            <div className='game-view'> 
+            </div>
+        </div>
+    )
+}
+
+export default HomeView;
