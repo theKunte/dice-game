fad04647e2add6e7edd9fffd9d9f17b108bb9d9d
Jenny <jennykunte90@gmail.com>
2023-04-17 20:39:16 -0700

    added xofAKind function'

--- yaahtzee-app/src/ScoreItem.js
+++ yaahtzee-app/src/ScoreItem.js
@@ -30,14 +30,49 @@ export const scoreSixes = (diceValues) => {
   return scoreValuesForUpperSection(diceValues, 6);
 };
 
-export const threeOfAKind = (diceValues) => {};
-export const fourOfAKind = (diceValues) => {};
+export const xOfAKind = (diceValues, x) => {
+  // [5,1,5,2,5]
+  let score = 0;
+  let counts = {};
+  let isXOfAKind = false;
+  for (let i = 0; i < diceValues.length; i++) {
+    score += diceValues[i];
+
+    if (counts[diceValues[i]]) {
+      counts[diceValues[i]] += 1;
+    } else {
+      counts[diceValues[i]] = 1;
+    }
+    if (counts[diceValues[i]] === x) {
+      isXOfAKind = true;
+    }
+  }
+  if (isXOfAKind) return score;
+  else return 0;
+};
+export const threeOfAKind = (diceValues) => {
+  return xOfAKind(diceValues, 3);
+};
+
+export const fourOfAKind = (diceValues) => {
+  return xOfAKind(diceValues, 4);
+};
 export const fullHouse = (diceValues) => {};
 
 export const smallStraight = (diceValues) => {};
 export const largeStraight = (diceValues) => {};
 
-export const yahtzee = (diceValues) => {};
+export const yahtzee = (diceValues) => {
+  // Lets check if all values in the array are the same
+  const firstValue = diceValues[0];
+  const allSame = diceValues.every((value) => value === firstValue);
+  if (allSame) {
+    // if all values are the same return yathzee score
+    return 50;
+  } else {
+    return 0;
+  }
+};
 
 export const chance = (diceValues) => {
   let score = 0;
