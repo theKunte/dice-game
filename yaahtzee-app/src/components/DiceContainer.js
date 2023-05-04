a6d1ac021b151f601465aaaa2f97090be0f1632e
Jenny <jennykunte90@gmail.com>
2023-05-04 15:59:07 -0700

    highlight scoreboard

--- yaahtzee-app/src/components/DiceContainer.js
+++ yaahtzee-app/src/components/DiceContainer.js
@@ -39,7 +39,7 @@ function DiceContainer(props) {
 
   return (
     <div>
-      {/* TODO: fix number of rolls. User should see that they have a total of 3 */}
+      {/* TODO:  fix number of rolls. User should see that they have a total of 3 */}
       <div className="roll-dice-bottom-view">
         <div className="roll-dice-button">
           <button
